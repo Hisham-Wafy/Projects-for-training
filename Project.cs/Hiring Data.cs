e0a05b2866a5d727a712328a54a5a1f8f5da53a3
Hisham_Wafy <m8ahmud7788@gmail.com>
2024-07-15 01:59:08 +0300

    add the class Hiring Data

---
+++ Project.cs/Hiring Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_OOP_Assignment
+{
+    internal class Hiring_Date
+    {
+       private int year, month, day;
+
+        #region Propertys
+        public int Day { get; set; }
+
+        public int Month { get; set; }
+
+        public int Year { get; set; } 
+        #endregion
+        public Hiring_Date(int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+    }
+}
